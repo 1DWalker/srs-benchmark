f7e8cf56180e31ab406035e6beef9b7b253fe1a3
1DWalker <25091361+1DWalker@users.noreply.github.com>
2025-05-29 02:59:10 -0400

    Add vertical lines

--- plot_forgetting_curve.py
+++ plot_forgetting_curve.py
@@ -1,14 +1,44 @@
+import random
 import lmdb
+from matplotlib import pyplot as plt
+import numpy as np
 import torch
 
 from full_db_config import FULL_DB_PATH
 from utils import load_tensor
 
-FILE_NAMES = ["RWKV", "FSRS-6-recency"]
+def fsrs6_forgetting_curve(t, s, decay):
+    factor = 0.9 ** (1 / -decay) - 1
+    return (1 + factor * t / s) ** -decay
 
+def rwkv_forgetting_curve(label_elapsed_seconds, w):
+    num_curves = 128
+    s_point_spread = 18.5
+    s_max = 22
+    s_space_raw = torch.exp(
+        torch.linspace(0, s_point_spread, num_curves, device=w.device)
+    )
+    s_space = 0.1 + (s_space_raw - 1) * (np.e ** (s_max - s_point_spread))
+    label_elapsed_seconds = torch.max(torch.tensor(1.0), label_elapsed_seconds)
+    return 1e-5 + (1 - 2 * 1e-5) * torch.sum(
+        w * 0.9 ** (label_elapsed_seconds / s_space), dim=-1
+    )
+
+@torch.inference_mode()
 def main():
     user_id = 1
     env = lmdb.open(FULL_DB_PATH, readonly=True, lock=False)
+    # info = env.info()
+    # stat = env.stat()
+
+    # allocated_space = info['map_size']  # in bytes
+    # used_space = (info['last_pgno'] + 1) * stat['psize']  # in bytes
+
+    # print(f"Allocated space: {allocated_space / (1024**2):.2f} MB")
+    # print(f"Used space: {used_space / (1024**2):.2f} MB")
+
+    # env.close()
+    # exit()
 
     with env.begin() as txn:
         print("start")
@@ -18,10 +48,6 @@ def main():
         card_ids = load_tensor(txn, f"{user_id}_card_id", device=torch.device("cpu")).numpy()
         ys = load_tensor(txn, f"{user_id}_y", device=torch.device("cpu")).numpy()
         elapsed_seconds_list = load_tensor(txn, f"{user_id}_elapsed_seconds", device=torch.device("cpu")).numpy()
-        print(rwkv_w)
-        print(fsrs_s)
-        print(rwkv_w.shape, fsrs_s.shape)
-        print(card_ids)
         card_id_locs = {}
         for i, card_id in enumerate(card_ids):
             if fsrs_s[i] != -1:
@@ -40,19 +66,73 @@ def main():
         #             print(k, v)
         #             exit()
 
-
         # card_id = 1725
-        card_id = 2558
-        locs = card_id_locs[card_id]
-        for i in locs:
-            print(i, fsrs_s[i], fsrs_decay[i], elapsed_seconds_list[i] / 86400, ys[i])
-            # print(rwkv_w[i])
+        # card_id = 2558
+        possible_card_ids = list(card_id_locs.keys())
+        possible_card_ids = [x for x in possible_card_ids if len(card_id_locs[x]) > 3]
+        random.shuffle(possible_card_ids)
+
+        possible_card_ids = [2379] * 100
+
+        for card_id in possible_card_ids[:10]:
+            print("Running", card_id)
+
+            locs = card_id_locs[card_id]
+            tot_time_seconds = 0
+            cumulative_seconds = []
+            for i in locs:
+                tot_time_seconds += elapsed_seconds_list[i]
+                cumulative_seconds.append(tot_time_seconds)
+                print(i, fsrs_s[i], fsrs_decay[i], elapsed_seconds_list[i] / 86400, ys[i])
+
+            cumulative_seconds = np.array(cumulative_seconds)
+
+            t_space = list(np.linspace(1, tot_time_seconds * 1.3, num=300))
+            # Add some extra values so RWKV's instant forgetting will appear & lapses aren't skipped
+            for t in cumulative_seconds:
+                if t - 0.01 > 1e-5:
+                    t_space.append(t - 0.01)
+                t_space.append(t + 0.01)
+                t_space.append(t + 0.1)
+            t_space.sort()
+            t_space = np.array(t_space)
+
+            fsrs_p = []
+            rwkv_p = []
+            for t_i, t in enumerate(t_space):
+                cum_i = 0
+                while cum_i < len(cumulative_seconds) and t > cumulative_seconds[cum_i]:
+                    cum_i += 1
+                cum_i -= 1
+                i = locs[cum_i]
+                fsrs_p.append(fsrs6_forgetting_curve((t - cumulative_seconds[cum_i]) / 86400, fsrs_s[i], fsrs_decay[i]))
+                rwkv_p.append(rwkv_forgetting_curve(torch.tensor(t - cumulative_seconds[cum_i]), rwkv_w[i]))
+
+            # Create the plot
+            plt.clf()
+            plt.plot(t_space / 86400, fsrs_p, label='FSRS-6-recency')
+            plt.plot(t_space / 86400, rwkv_p, label='RWKV')
+            for i, review_time_seconds in zip(locs[1:], cumulative_seconds[1:]):
+                # Require half a day
+                if elapsed_seconds_list[i] < 60*60*12:
+                    continue
+                color = 'r' if ys[i] == 0 else 'g'
+                print(i, ys[i], color)
+                plt.axvline(x=review_time_seconds / 86400, color=color, linestyle='--')
 
 
+            # Add labels and title
+            plt.xlabel('Days')
+            plt.ylabel('R')
+            plt.title(f'predicted R vs time. User: {user_id}, Card_id: {card_id}')
 
-        # with txn.cursor() as cursor:
-        #     for key in cursor.iternext(keys=True, values=False):
-        #         print(key)
+            # Add legend
+            plt.legend()
+
+            # Show the plot
+            plt.show()
+            exit()
+            # plt.savefig(f'plots/forgetting-curves/{user_id}_{card_id}.png', dpi=600)
 
 if __name__ == '__main__':
     main()
