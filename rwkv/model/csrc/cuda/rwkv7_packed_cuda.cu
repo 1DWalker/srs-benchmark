25132fb6193544d21c223fe4650f749d8a1b2861
1DWalker <25091361+1DWalker@users.noreply.github.com>
2025-06-02 17:12:12 -0400

    Fix names

--- rwkv/model/csrc/cuda/rwkv7_packed_cuda.cu
+++ rwkv/model/csrc/cuda/rwkv7_packed_cuda.cu
@@ -12,86 +12,84 @@ using namespace nvcuda;
 
 namespace rwkv {
 template <int CHUNK_LEN=32, typename F>
-std::tuple<at::Tensor, at::Tensor> rwkv7_wkv_forward_cuda(
+std::tuple<at::Tensor, at::Tensor> rwkv7_packed_wkv_forward_cuda(
     const at::Tensor& indices_I,
-    const at::Tensor& r_BTHK, 
-    const at::Tensor& k_BTHK,
-    const at::Tensor& v_BTHK,
-    const at::Tensor& w_BTHK,
-    const at::Tensor& a_BTHK,
-    const at::Tensor& k_deformed_BTHK
+    const at::Tensor& r_THK, 
+    const at::Tensor& k_THK,
+    const at::Tensor& v_THK,
+    const at::Tensor& w_THK,
+    const at::Tensor& a_THK,
+    const at::Tensor& k_deformed_THK
     ) {
-    const int B = r_BTHK.size(0);
-    const int T = r_BTHK.size(1);
-    const int H = r_BTHK.size(2);
-    const int K = r_BTHK.size(3);
-    TORCH_INTERNAL_ASSERT(r_BTHK.device().type() == at::DeviceType::CUDA);
-    const F* r_ptr = (F*)r_BTHK.data_ptr();
-    const F* k_ptr = (F*)k_BTHK.data_ptr();
-    const F* v_ptr = (F*)v_BTHK.data_ptr();
-    const float* w_ptr = w_BTHK.data_ptr<float>();
-    const F* a_ptr = (F*)a_BTHK.data_ptr();
-    const F* k_deformed_ptr = (F*)k_deformed_BTHK.data_ptr();
-    
-    at::Tensor out_BTHK = torch::empty(r_BTHK.sizes(), r_BTHK.options());
-    F* out_ptr = (F*)out_BTHK.data_ptr();
-    int L = (T + CHUNK_LEN) / CHUNK_LEN;
-    at::Tensor state_checkpoints_BLHKK = torch::empty({B, L, H, K, K}, r_BTHK.options().dtype(torch::kFloat32)).requires_grad_(false);
-    float* state_checkpoints_ptr = state_checkpoints_BLHKK.data_ptr<float>();
     printf("Here forward\n");
-    return std::make_tuple(out_BTHK, state_checkpoints_BLHKK);
+    const int T = r_THK.size(0);
+    const int H = r_THK.size(1);
+    const int K = r_THK.size(2);
+    TORCH_INTERNAL_ASSERT(r_THK.device().type() == at::DeviceType::CUDA);
+    const F* r_ptr = (F*)r_THK.data_ptr();
+    const F* k_ptr = (F*)k_THK.data_ptr();
+    const F* v_ptr = (F*)v_THK.data_ptr();
+    const float* w_ptr = w_THK.data_ptr<float>();
+    const F* a_ptr = (F*)a_THK.data_ptr();
+    const F* k_deformed_ptr = (F*)k_deformed_THK.data_ptr();
+    
+    at::Tensor out_THK = torch::empty(r_THK.sizes(), r_THK.options());
+    F* out_ptr = (F*)out_THK.data_ptr();
+    int L = (T + CHUNK_LEN) / CHUNK_LEN;
+    at::Tensor state_checkpoints_LHKK = torch::empty({L, H, K, K}, r_THK.options().dtype(torch::kFloat32)).requires_grad_(false);
+    float* state_checkpoints_ptr = state_checkpoints_LHKK.data_ptr<float>();
+    return std::make_tuple(out_THK, state_checkpoints_LHKK);
 }
 
 template <int CHUNK_LEN=32, typename F>
-std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor> rwkv7_wkv_backward_cuda(
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor> rwkv7_packed_wkv_backward_cuda(
     const at::Tensor& indices_I,
-    const at::Tensor& r_BTHK, 
-    const at::Tensor& k_BTHK,
-    const at::Tensor& v_BTHK,
-    const at::Tensor& w_BTHK,
-    const at::Tensor& a_BTHK,
-    const at::Tensor& k_deformed_BTHK,
-    const at::Tensor& state_checkpoints_BLHKK,
-    const at::Tensor& grad_BTHK
+    const at::Tensor& r_THK, 
+    const at::Tensor& k_THK,
+    const at::Tensor& v_THK,
+    const at::Tensor& w_THK,
+    const at::Tensor& a_THK,
+    const at::Tensor& k_deformed_THK,
+    const at::Tensor& state_checkpoints_LHKK,
+    const at::Tensor& grad_THK
     ) {
-    const int B = r_BTHK.size(0);
-    const int T = r_BTHK.size(1);
-    const int H = r_BTHK.size(2);
-    const int K = r_BTHK.size(3);
-    const int L = state_checkpoints_BLHKK.size(1);
-    TORCH_INTERNAL_ASSERT(r_BTHK.device().type() == at::DeviceType::CUDA);
-    const F* r_ptr = (F*)r_BTHK.data_ptr();
-    const F* k_ptr = (F*)k_BTHK.data_ptr();
-    const F* v_ptr = (F*)v_BTHK.data_ptr();
-    const float* w_ptr = w_BTHK.data_ptr<float>();
-    const F* a_ptr = (F*)a_BTHK.data_ptr();
-    const F* k_deformed_ptr = (F*)k_deformed_BTHK.data_ptr();
-    const float* state_checkpoints_ptr = state_checkpoints_BLHKK.data_ptr<float>();
-    const F* grad_ptr = (F*)grad_BTHK.data_ptr();
-    at::Tensor r_grad_BTHK = torch::zeros_like(r_BTHK);
-    at::Tensor k_grad_BTHK = torch::zeros_like(r_BTHK);
-    at::Tensor v_grad_BTHK = torch::zeros_like(r_BTHK);
-    at::Tensor w_grad_BTHK = torch::zeros_like(r_BTHK, torch::dtype(torch::kFloat32));
-    at::Tensor a_grad_BTHK = torch::zeros_like(r_BTHK);
-    at::Tensor k_deformed_grad_BTHK = torch::zeros_like(r_BTHK);
-    F* r_grad_ptr = (F*)r_grad_BTHK.data_ptr();
-    F* k_grad_ptr = (F*)k_grad_BTHK.data_ptr();
-    F* v_grad_ptr = (F*)v_grad_BTHK.data_ptr();
-    float* w_grad_ptr = w_grad_BTHK.data_ptr<float>();
-    F* a_grad_ptr = (F*)a_grad_BTHK.data_ptr();
-    F* k_deformed_grad_ptr = (F*)k_deformed_grad_BTHK.data_ptr();
-
     printf("Here backwar\n");
-    return std::make_tuple(r_grad_BTHK, k_grad_BTHK, v_grad_BTHK, w_grad_BTHK, a_grad_BTHK, k_deformed_grad_BTHK);
+    const int T = r_THK.size(0);
+    const int H = r_THK.size(1);
+    const int K = r_THK.size(2);
+    const int L = state_checkpoints_LHKK.size(1);
+    TORCH_INTERNAL_ASSERT(r_THK.device().type() == at::DeviceType::CUDA);
+    const F* r_ptr = (F*)r_THK.data_ptr();
+    const F* k_ptr = (F*)k_THK.data_ptr();
+    const F* v_ptr = (F*)v_THK.data_ptr();
+    const float* w_ptr = w_THK.data_ptr<float>();
+    const F* a_ptr = (F*)a_THK.data_ptr();
+    const F* k_deformed_ptr = (F*)k_deformed_THK.data_ptr();
+    const float* state_checkpoints_ptr = state_checkpoints_LHKK.data_ptr<float>();
+    const F* grad_ptr = (F*)grad_THK.data_ptr();
+    at::Tensor r_grad_THK = torch::zeros_like(r_THK);
+    at::Tensor k_grad_THK = torch::zeros_like(r_THK);
+    at::Tensor v_grad_THK = torch::zeros_like(r_THK);
+    at::Tensor w_grad_THK = torch::zeros_like(r_THK, torch::dtype(torch::kFloat32));
+    at::Tensor a_grad_THK = torch::zeros_like(r_THK);
+    at::Tensor k_deformed_grad_THK = torch::zeros_like(r_THK);
+    F* r_grad_ptr = (F*)r_grad_THK.data_ptr();
+    F* k_grad_ptr = (F*)k_grad_THK.data_ptr();
+    F* v_grad_ptr = (F*)v_grad_THK.data_ptr();
+    float* w_grad_ptr = w_grad_THK.data_ptr<float>();
+    F* a_grad_ptr = (F*)a_grad_THK.data_ptr();
+    F* k_deformed_grad_ptr = (F*)k_deformed_grad_THK.data_ptr();
+
+    return std::make_tuple(r_grad_THK, k_grad_THK, v_grad_THK, w_grad_THK, a_grad_THK, k_deformed_grad_THK);
 }
 
 const int CHECKPOINT_LEN = 32;
 TORCH_LIBRARY_IMPL(rwkv, CUDA, m) {
-    m.impl("rwkv7_packed_wkv_forward_float", &rwkv7_wkv_forward_cuda<CHECKPOINT_LEN, float>);
-    m.impl("rwkv7_packed_wkv_backward_float", &rwkv7_wkv_backward_cuda<CHECKPOINT_LEN, float>);
-    m.impl("rwkv7_packed_wkv_forward_bfloat16", &rwkv7_wkv_forward_cuda<CHECKPOINT_LEN, __nv_bfloat16>);
-    m.impl("rwkv7_packed_wkv_backward_bfloat16", &rwkv7_wkv_backward_cuda<CHECKPOINT_LEN, __nv_bfloat16>);
-    m.impl("rwkv7_packed_wkv_forward_half", &rwkv7_wkv_forward_cuda<CHECKPOINT_LEN, __half>);
-    m.impl("rwkv7_packed_wkv_backward_half", &rwkv7_wkv_backward_cuda<CHECKPOINT_LEN, __half>);
+    m.impl("rwkv7_packed_wkv_forward_float", &rwkv7_packed_wkv_forward_cuda<CHECKPOINT_LEN, float>);
+    m.impl("rwkv7_packed_wkv_backward_float", &rwkv7_packed_wkv_backward_cuda<CHECKPOINT_LEN, float>);
+    m.impl("rwkv7_packed_wkv_forward_bfloat16", &rwkv7_packed_wkv_forward_cuda<CHECKPOINT_LEN, __nv_bfloat16>);
+    m.impl("rwkv7_packed_wkv_backward_bfloat16", &rwkv7_packed_wkv_backward_cuda<CHECKPOINT_LEN, __nv_bfloat16>);
+    m.impl("rwkv7_packed_wkv_forward_half", &rwkv7_packed_wkv_forward_cuda<CHECKPOINT_LEN, __half>);
+    m.impl("rwkv7_packed_wkv_backward_half", &rwkv7_packed_wkv_backward_cuda<CHECKPOINT_LEN, __half>);
 }
 }
