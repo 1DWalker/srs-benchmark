6d2e9bb4d99e65548bf94c64b89d91357545f03f
1DWalker <25091361+1DWalker@users.noreply.github.com>
2025-06-25 14:49:02 -0400

    Multiprocess

--- fsrs_cpp/run.py
+++ fsrs_cpp/run.py
@@ -1,11 +1,17 @@
+import json
+from pathlib import Path
+import queue
 import time
 import lmdb
 import numpy as np
 from sklearn.metrics import log_loss
 import torch
+from tqdm import tqdm
 from fsrs_cpp.fsrs6_reference import FSRS6
 from fsrs_cpp.fsrs_ops import FSRSCppFunction, RevlogTensors
+from script import sort_jsonl
 from utils import load_tensor, parse_toml
+import multiprocessing as mp
 
 def fsrs_fun(params, review_th_batch, revlog_tensors):
     return FSRSCppFunction.apply(params, review_th_batch, revlog_tensors)
@@ -16,11 +22,10 @@ def train_eval(params, train_set_review_ths, revlog_tensors: RevlogTensors):
         y = (revlog_tensors.packed_rating_T[revlog_tensors.perm_inv_T_tensor[train_set_review_ths - 1]] > 1).float()
         return log_loss(y_true=y, y_pred=pred_y, labels=[0, 1])
 
-def process(config, user_id):
+def process(config, user_id, env):
     print("Process:", user_id)
     fsrs_reference = FSRS6()
 
-    env = lmdb.open(config.DB_PATH, readonly=True, lock=False)
     with env.begin(write=False) as txn:
         device = torch.device("cpu")
         start = time.time()
@@ -100,24 +105,105 @@ def process(config, user_id):
                 best_loss = eval_loss
                 best_params = params.detach().clone()
 
-            print("Best params:", best_params)
             test_pred_y = fsrs_fun(best_params, test_set_review_ths, revlog_tensors)
             test_y = y_T[test_set_review_ths - 1]
             test_pred_y_all.extend(test_pred_y.detach().numpy())
             test_y_all.extend(test_y.numpy())
 
         logloss = log_loss(y_true=test_y_all, y_pred=test_pred_y_all, labels=[0, 1])
-        print("Log loss:", logloss, "size:", len(test_y_all))
+        return {
+            "metrics": {
+                "LogLoss": round(logloss, 6)
+            },
+            "user": int(user_id),
+            "size": len(test_y_all),
+            "parameters": {
+                "0": list(map(lambda x: round(x, 4), best_params.tolist()))
+            },
+        }
+
+
+def worker_job(config, job_queue, writer_queue, progress_queue):
+    env = lmdb.open(config.DB_PATH, readonly=True, lock=False)
+    while True:
+        try:
+            user = job_queue.get_nowait()
+            if user is None:
+                return
+
+            writer_queue.put(process(config, user, env))
+            progress_queue.put(1)
+        except queue.Empty:
+            return
+
+def writer_job(result_file, writer_queue):
+    while True:
+        message = writer_queue.get()
+        if message is None:
+            return
+        stats = message
+        with open(result_file, "a") as f:
+            f.write(json.dumps(stats, ensure_ascii=False) + "\n")
+    
+def progress_tracker(total_items, progress_queue):
+    with tqdm(total=total_items, desc="Progress") as pbar:
+        for _ in range(total_items):
+            progress_queue.get()
+            pbar.update(1)
 
 def main(config):
-    USER_IDS = list(range(config.USER_START, config.USER_END + 1))
-    if 4371 in USER_IDS:
-        USER_IDS.remove(4371)
-        print("Removed user 4371.")
+    mp.set_start_method("spawn", force=True)
+    unprocessed_users = []
+    Path("result").mkdir(parents=True, exist_ok=True)
+    result_file = Path(f"result/{config.OUT_NAME}.jsonl")
+    if result_file.exists():
+        data = sort_jsonl(result_file)
+        processed_user = set(map(lambda x: x["user"], data))
+    else:
+        processed_user = set()
 
-    for user in USER_IDS:
-        process(config, user)
-    
+    users = list(range(config.USER_START, config.USER_END + 1))
+    if 4371 in users:
+        users.remove(4371)
+        print("Removed user 4371 from users.")
+    for user_id in users:
+        if user_id not in processed_user:
+            unprocessed_users.append(user_id)
+
+    unprocessed_users.sort()
+    print("Unprocessed users length:", len(unprocessed_users))
+
+    job_queue = mp.Queue()
+    for user_id in users:
+        job_queue.put(user_id)
+
+    with mp.Manager() as manager:
+        writer_queue = manager.Queue()
+        writer = mp.Process(
+            target=writer_job, args=(result_file, writer_queue)
+        )
+        writer.start()
+
+        progress_queue = manager.Queue()
+        progress_process = mp.Process(
+            target=progress_tracker, args=(len(unprocessed_users), progress_queue)
+        )
+        progress_process.start()
+
+        jobs = [mp.Process(
+            target=worker_job, args=(config, job_queue, writer_queue, progress_queue)
+        ) for _ in range(config.PROCESSES)]
+        for job in jobs:
+            job.start()
+
+        for job in jobs:
+            job.join()
+
+        writer_queue.put(None)
+        writer.join()
+        progress_process.terminate()
+
+    sort_jsonl(result_file)
 
 if __name__ == '__main__':
     config = parse_toml()
