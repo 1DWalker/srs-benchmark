40bd26d9c0168c5b372a4453a634844e19f9dc2c
unknown <25091361+1DWalker@users.noreply.github.com>
2026-01-12 12:08:33 -0500

    add fsrs-7

--- models/fsrs_v7.py
+++ models/fsrs_v7.py
@@ -51,14 +51,15 @@ class FSRS7ParameterClipper(FSRS6ParameterClipper):
             w[25] = w[25].clamp(2.5, 10)
             w[26] = w[26].clamp(0, 1)
             # Forgetting curve
-            w[27] = w[27].clamp(0.005, 0.1)  # delta_t<S part of the curve
-            w[28] = w[28].clamp(0.1, 0.3)  # delta_t>S part of the curve
-            # blend_start
-            w[29] = w[29].clamp(0, 0.79)  # "ceiling" of blend_start
-            w[30] = w[30].clamp(0, w[29])  # "floor" of blend_start
-            w[31] = w[31].clamp(0.1, 5)  # slope for D
-            w[32] = w[32].clamp(0, 10)  # intercept for D
-            w[33] = w[33].clamp(0.01, 1)  # power for 1+S
+            w[27] = w[27].clamp(0.01, 0.99)  # decay 1
+            w[28] = w[28].clamp(0.01, 0.99)  # decay 2
+            w[29] = w[29].clamp(0.01, 0.99)  # decay 3
+            w[30] = w[30].clamp(0.5, 0.99)  # base 1
+            w[31] = w[31].clamp(0.5, 0.99)  # base 2
+            w[32] = w[32].clamp(0.5, 0.99)  # base 3
+            w[33] = w[33].clamp(0.001, 1)  # weight 1
+            w[34] = w[34].clamp(0.001, 1)  # weight 2
+            w[35] = w[35].clamp(0.001, 1)  # weight 3
             module.w.data = w
 
 
@@ -68,26 +69,13 @@ class FSRS7(FSRS6):
     lr: float = 2e-2
     betas: tuple = (0.8, 0.85)  # this is for Adam, default is (0.9, 0.999)
 
-    # Old config
-    # n_epoch: int = 5
-    # batch_size: int = 512
-    # lr: float = 4e-2
-
     # Multi-user optimization
     init_w = [0.0002, 1.0551, 2.5994, 12.6293,  # Initial S
               4.3654, 0.4526, 3.3309,  # Difficulty
               1.1151, 0.2171, 1.7041, 0.0623, 0.001, 0.985, 0.0, 0.6379, 1.2916,  # Stability (long-term)
-              1.4172, 0.3211, 3.7526, 0.001, 0.2414, 0.001, 1.5227, 0.2113, 1.0,  # Stability (short-term)
+              1.4172, 0.3211, 3.7526, 0.001, 0.2414, 0.001, 1.5227, 0.2113, 1.3,  # Stability (short-term)
               2.5, 0.998,  # Long-short term transition function
-              0.0053, 0.1102, 0.7899, 0.4503, 5.0, 6.7423, 0.2353]
-
-    # default_params_stddev_tensor = torch.tensor([9999., 9999., 9999., 9999.,  # Initial S
-    #                                              9999., 9999., 9999.,  # Difficulty
-    #                                              9999., 9999., 9999., 9999., 9999., 9999., 9999., 9999., 9999.,  # Stability (long-term)
-    #                                              9999., 9999., 9999., 9999., 9999., 9999., 9999., 9999., 9999.,  # Stability (short-term)
-    #                                              9999., 9999.,  # Long-short term transition function
-    #                                              9999., 9999.,  # Forgetting curve
-    #                                              9999., 9999., 9999., 9999., 9999., 9999.])
+              0.12, 0.04, 0.7, 0.9, 0.7, 0.95, 0.5, 0.75, 0.2]
 
     def __init__(self, config: Config, w: Optional[List[float]] = None):
         super().__init__(config)
@@ -110,42 +98,33 @@ class FSRS7(FSRS6):
             torch.arange(real_batch_size, device=self.config.device),
         ].transpose(0, 1)
 
-        blend_start = self._blend_start(difficulties, self.w[-5], self.w[-4], self.w[-3], self.w[-2])
-        retentions = self.forgetting_curve(delta_ts, stabilities, blend_start, -self.w[-7], -self.w[-6], -self.w[-1])
+        retentions = self.forgetting_curve(delta_ts, stabilities,
+                                           -self.w[-9], -self.w[-8], -self.w[-7],
+                                           self.w[-6], self.w[-5], self.w[-4],
+                                           self.w[-3], self.w[-2], self.w[-1])
         retentions = retentions.clamp(0.0001, 0.9999)
         output = {
             "retentions": retentions,
             "stabilities": stabilities,
             "difficulties": difficulties,
         }
-        # output["penalty"] = (
-        #     torch.sum(
-        #         torch.square(self.w - self.init_w_tensor)
-        #         / torch.square(self.default_params_stddev_tensor)
-        #     )
-        #     * real_batch_size
-        #     * self.gamma
-        # )
         return output
 
-    def _blend_start(self, d: Union[float, Tensor],
-                     blend_start_ceil=init_w[-5],
-                     blend_start_floor=init_w[-4],
-                     d_slope=init_w[-3],
-                     d_intercept=init_w[-2]) -> Tensor:
-        f_d = 2.718281828459045 ** (-d_slope * (d - d_intercept))
-        output = (blend_start_ceil - blend_start_floor) / (1 + f_d)
-        return output + blend_start_floor
-
-    def forgetting_curve(self, t, s, blend_start, decay1=-init_w[-7], decay2=-init_w[-6], s_pow=-init_w[-1]):
-        factor1 = 0.9 ** (1 / decay1) - 1
-        factor2 = 0.9 ** (1 / decay2) - 1
+    def forgetting_curve(self, t, s,
+                         decay1=-init_w[-9], decay2=-init_w[-8], decay3=-init_w[-7],
+                         base1=init_w[-6], base2=init_w[-5], base3=init_w[-4],
+                         weight1=init_w[-3], weight2=init_w[-2], weight3=init_w[-1]):
+        # decays must be passed into forgetting_curve with a minus sign
+        factor1 = base1 ** (1 / decay1) - 1
+        factor2 = base2 ** (1 / decay2) - 1
+        factor3 = base3 ** (1 / decay3) - 1
         t_over_s = t / s
         R1 = (1 + factor1 * t_over_s) ** decay1
         R2 = (1 + factor2 * t_over_s) ** decay2
-        s_factor = (1 + s) ** s_pow
-        blending_function = (blend_start * s_factor) * 2.718281828459045 ** (-0.1 * t_over_s)
-        return blending_function * R1 + (1 - blending_function) * R2
+        R3 = (1 + factor3 * t_over_s) ** decay3
+        numerator = weight1 * R1 + weight2 * R2 + weight3 * R3
+        denominator = weight1 + weight2 + weight3
+        return numerator / denominator
 
     def stability_after_success_long_term(self, state: Tensor, r: Tensor, rating: Tensor) -> Tensor:  # type: ignore[override]
         hard_penalty = torch.where(rating == 2, self.w[14], 1)
@@ -284,14 +263,9 @@ class FSRS7(FSRS6):
         average_recall = train_set["y"].mean()
         r_s0_default = {str(i): self.init_w[i - 1] for i in range(1, 5)}
 
-        def init_d_pretrain(rating: int, w_4: float, w_5: float):
-            new_d = w_4 - np.exp(w_5 * (rating - 1)) + 1
-            return new_d
-
         def evaluate_param_set(param_set):
             """Evaluate a parameter set and return total loss and rating stabilities"""
-            decay1, decay2, blend_start_ceil, blend_start_floor, d_slope, d_intercept, s_pow, w_4, w_5 = \
-                param_set
+            decay1, decay2, decay3, base1, base2, base3, weight1, weight2, weight3 = param_set
             current_rating_stability = {}
             current_rating_count = {}
             total_loss = 0
@@ -315,24 +289,18 @@ class FSRS7(FSRS6):
 
                 init_s0 = r_s0_default[first_rating]
 
-                d_default = init_d_pretrain(int(first_rating), w_4, w_5)
-                d_default = np.clip(d_default, 1, 10)
-
                 def loss(stability):
                     assert first_rating in ["1", "2", "3", "4"]
-                    blend_start = self._blend_start(d_default, blend_start_ceil, blend_start_floor, d_slope, d_intercept)
-                    # Sanity checks
-                    assert blend_start_ceil >= blend_start_floor
-                    assert decay1 <= decay2
-                    assert 0.01 <= s_pow <= 1.
-                    assert (1 + stability) ** -s_pow < 1
-                    y_pred = self.forgetting_curve(delta_t, stability, blend_start, -decay1, -decay2, -s_pow)
+                    y_pred = self.forgetting_curve(delta_t, stability,
+                                                   -decay1, -decay2, -decay3,
+                                                   base1, base2, base3,
+                                                   weight1, weight2, weight3)
                     y_pred = np.clip(y_pred, 0.0001, 0.9999)
                     logloss = sum(
                         -(recall * np.log(y_pred) + (1 - recall) * np.log(1 - y_pred))
                         * count
                     )
-                    l1 = (np.abs(stability - init_s0)) / 32
+                    l1 = (np.abs(stability - init_s0)) / 16
                     return logloss + l1
 
                 res = minimize(
@@ -362,22 +330,8 @@ class FSRS7(FSRS6):
 
         # Initial parameter sets (grid search)
         initial_forgetting_curve_params = [
-            self.init_w[-7:] + self.init_w[4:6],
-            # decay1, decay2, blend_start_ceil, blend_start_floor, d_slope, d_intercept, s_pow, w_4, w_5
-            [0.005, 0.178, 0.7161, 0.3981, 3.52, 2.968, 0.5148, 4.1579, 0.4291],
-            [0.005, 0.1185, 0.7547, 0.3769, 3.6524, 2.9115, 0.2838, 4.2913, 0.53],
-            [0.005, 0.1372, 0.79, 0.4012, 3.5494, 3.0804, 0.1446, 4.611, 0.6235],
-            [0.005, 0.1, 0.79, 0.4045, 3.7489, 2.8624, 0.2144, 4.7442, 0.4859],
-            [0.005, 0.1233, 0.79, 0.3538, 3.7158, 2.6577, 0.2826, 4.6638, 0.4589],
-            [0.005, 0.1846, 0.79, 0.5172, 3.496, 2.7762, 0.132, 5.0726, 0.5166],
-            [0.005, 0.1184, 0.79, 0.5439, 3.7936, 2.8059, 0.2543, 4.5834, 0.5916],
-            [0.005, 0.1763, 0.7595, 0.3978, 3.6942, 2.9052, 0.1569, 4.2623, 0.5103],
-            [0.005, 0.1839, 0.7561, 0.4849, 3.5836, 2.9189, 0.3721, 4.1967, 0.4184],
-            [0.005, 0.1518, 0.79, 0.3663, 3.6678, 2.9241, 0.4017, 4.1075, 0.5569],
-            [0.005, 0.1722, 0.79, 0.2255, 3.9109, 3.2016, 0.2979, 4.6527, 0.5054],
-            [0.005, 0.1353, 0.79, 0.5503, 3.6498, 2.6347, 0.2889, 4.7851, 0.456],
-            [0.005, 0.1072, 0.79, 0.4332, 3.4019, 2.7145, 0.2215, 4.4336, 0.4954],
-            [0.005, 0.1217, 0.7342, 0.443, 3.4413, 2.9602, 0.4536, 4.1251, 0.4555]
+            # forgetting curves params
+            self.init_w[-9:],
         ]
 
         # Track all candidates with their losses
@@ -493,9 +447,7 @@ class FSRS7(FSRS6):
         # Update forgetting curve parameters with the best found parameters
         if best_forgetting_curve_params is not None:
             # forgetting_curve
-            self.w.data[-7:] = Tensor(best_forgetting_curve_params[:-2])
-            # init_d
-            self.w.data[4:6] = Tensor(best_forgetting_curve_params[-2:])
+            self.w.data[-9:] = Tensor(best_forgetting_curve_params)
 
         self.init_w_tensor = self.w.data.clone().to(self.config.device)
 
@@ -519,9 +471,10 @@ class FSRS7(FSRS6):
             new_d = new_d.clamp(1, 10)
         else:
             success = X[:, 1] > 1
-            # The initial steep drop-off is larger if the last rating was Again
-            blend_start = self._blend_start(state[:, 1], self.w[-5], self.w[-4], self.w[-3], self.w[-2])
-            r = self.forgetting_curve(X[:, 0], state[:, 0], blend_start, -self.w[-7], -self.w[-6], -self.w[-1])
+            r = self.forgetting_curve(X[:, 0], state[:, 0],
+                                           -self.w[-9], -self.w[-8], -self.w[-7],
+                                           self.w[-6], self.w[-5], self.w[-4],
+                                           self.w[-3], self.w[-2], self.w[-1])
 
             if not torch.isfinite(r).all():
                 print('R contains NaN/Inf')
@@ -559,5 +512,5 @@ class FSRS7(FSRS6):
         new_s = new_s.clamp(self.config.s_min, 36500)
         return torch.stack([new_s, new_d], dim=1)
 
-    def get_optimizer(self, lr: float, wd: float, betas) -> torch.optim.Optimizer:
-        return torch.optim.NAdam(self.parameters(), lr=lr, betas=betas)
+    def get_optimizer(self, lr, wd, betas):
+        return torch.optim.NAdam(params=self.parameters(), lr=lr, weight_decay=wd, betas=betas)
