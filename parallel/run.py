f7424cc129e8f407e02b1eb0a6fab218730eac5e
1DWalker <25091361+1DWalker@users.noreply.github.com>
2026-05-17 02:43:43 -0400

    fix batch

--- parallel/run.py
+++ parallel/run.py
@@ -10,9 +10,10 @@ from parallel.config import (
     LMDB_PATH,
     LMDB_SIZE,
     DEVICE,
-    TEST_PARALLEL,
+    N_SPLITS,
+    TEST_BATCH_SIZE_MAX,
 )
-from parallel.tensors import ConcatTensors, ReviewData, UserTensorBlob
+from parallel.tensors import ConcatTensors, ParamKey, ReviewData, UserTensorBlob
 
 def load_blob_bytes(blob_bytes: bytes) -> UserTensorBlob:
     buffer = BytesIO(blob_bytes)
@@ -36,19 +37,44 @@ def load_user_blob(txn: lmdb.Transaction, user_id: int) -> UserTensorBlob:
         raise LookupError(f"Packed blob not found for user {user_id}.")
     return load_blob_bytes(blob_bytes)
 
-def predict(review_data: ReviewData, index, param_key, params):
+def predict(review_data: ReviewData, index, params):
+    assert index.size(0) == params.size(0)
+    seq_lens = review_data.seq_len[index]
+    max_seq_len = seq_lens.max()
+    print(seq_lens)
+    print(max_seq_len)
     pass
     
 
 def evaluate_on_test_set(fsrs_params: torch.Tensor, data: ConcatTensors):
-    # get the right params for each test index
-    # test_p = predict(data.review_data, data.test_index, data.test_index_param_key, fsrs_params)
-    param_key = data.get_test_index_param_key()
-    parallel_n = TEST_PARALLEL
-
     print("eval on test")
+    param_keys = data.get_test_index_param_key()
 
+    test_seq_len = data.review_data.seq_len[data.test_index]
+    sorted_test_index_permutation = torch.argsort(test_seq_len)
+    # sorted_test_index = data.test_index[sorted_test_index_permutation]
+    # sorted_param_key = ParamKey(
+    #     user_index=unsorted_param_key.user_index[sorted_test_index_permutation],
+    #     split_index=unsorted_param_key.split_index[sorted_test_index_permutation],
+    # )
+    restore_test_index_permutation = torch.empty_like(sorted_test_index_permutation)
+    restore_test_index_permutation[sorted_test_index_permutation] = torch.arange(
+        sorted_test_index_permutation.numel(),
+        device=sorted_test_index_permutation.device,
+    )
     print("sort done")
+
+    def ceil_div(a: int, b: int) -> int:
+        return (a + b - 1) // b
+
+    N = data.test_index.size(0)
+    num_batches = ceil_div(N, TEST_BATCH_SIZE_MAX)
+    batch_size = ceil_div(N, num_batches)
+    for perm_slice in sorted_test_index_permutation.split(batch_size):
+        batch = test_seq_len[perm_slice]
+        batch_fsrs_params = fsrs_params[param_keys.user_index[batch], param_keys.split_index[batch]]
+        p = predict(data.review_data, batch, batch_fsrs_params)
+    
 
 def run(
     env: lmdb.Environment,
@@ -60,9 +86,12 @@ def run(
             for user_id in tqdm(users, total=len(users), smoothing=0.03, desc="Loading user data")
         ]
 
+    # TODO sort users by train split length
+    # TODO train
+
     print(blobs)
 
-    fsrs_params = torch.randn((len(users), 2, 35))
+    fsrs_params = torch.zeros((len(users), N_SPLITS, 35), device=DEVICE)
     data = ConcatTensors(blobs)
     print(data)
 
