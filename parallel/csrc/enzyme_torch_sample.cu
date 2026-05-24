8d9bbe1a4a4dc1d30e3869a8c71c8501856cdfea
1DWalker <25091361+1DWalker@users.noreply.github.com>
2026-05-24 19:18:11 -0400

    Move test code

--- parallel/csrc/enzyme_torch_sample.cu
+++ parallel/csrc/enzyme_torch_sample.cu
@@ -1,7 +1,7 @@
 #include <cuda_runtime.h>
 #include <stdint.h>
 
-#include "fsrs/fsrs7.cu"
+#include "fsrs/fsrs_test.cu"
 
 
 __device__ void square_impl(double* x, double* y) {
@@ -53,65 +53,6 @@ __global__ void square_backward_kernel(
     );
 
     grad_x[i] = grad_x_local;
-}
-
-__global__ void fsrs_test_kernel(
-    const float* __restrict__ elapsed_days_real_flat,
-    const int8_t* __restrict__ rating_flat,
-    const int32_t* __restrict__ start_index,
-    const int32_t* __restrict__ seq_len,
-    const fsrs_params_t* __restrict__ fsrs_params,
-    const int32_t N,
-    float* __restrict__ p
-) {
-    int64_t i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i >= N) return;
-
-    const int32_t start = start_index[i];
-    const int32_t len = seq_len[i];
-    const fsrs_params_t params = fsrs_params[i];
-
-    fsrs_state_t state = fsrs7_init(params, rating_flat[start]);
-    for (int32_t l = 1; l < len - 1; ++l) {
-        const int32_t review_index = start + l;
-        state = fsrs7_step(
-            params,
-            state,
-            elapsed_days_real_flat[review_index],
-            rating_flat[review_index]
-        );
-    }
-
-    const int32_t target_index = start + len - 1;
-    // std::cout << state.s << ' ' << elapsed_days_real_flat[target_index] << '\n';
-    p[i] = fsrs7_forgetting_curve(
-        params,
-        elapsed_days_real_flat[target_index],
-        state.s
-    );
-}
-
-extern "C" void fsrs_test_cuda(
-    const float* __restrict__  elapsed_days_real_flat,
-    const int8_t* __restrict__  rating_flat,
-    const int32_t* __restrict__  start_index,
-    const int32_t* __restrict__  seq_len,
-    const fsrs_params_t* __restrict__  fsrs_params,
-    float* __restrict__  p,
-    const int32_t N,
-    cudaStream_t stream
-) {
-    constexpr int threads = 256;
-    int blocks = static_cast<int>((N + threads - 1) / threads);
-    fsrs_test_kernel<<<blocks, threads, 0, stream>>>(
-        elapsed_days_real_flat,
-        rating_flat,
-        start_index,
-        seq_len,
-        fsrs_params,
-        N,
-        p
-    );
 }
 
 extern "C" void enzyme_square_backward_cuda(
